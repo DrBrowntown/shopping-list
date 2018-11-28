e8a119bf850d84c14a51d2b21d4ad20336941962
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-28 16:09:42 -0500

    feat: Send item:clear when clear items submenu is clicked

--- main.js
+++ main.js
@@ -71,7 +71,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: "Clear items"
+        label: "Clear items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
       },
       {
         label: "Quit",
