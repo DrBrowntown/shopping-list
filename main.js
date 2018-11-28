432889352839533d46e5fec91fdf7042572bfb7c
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-28 15:06:25 -0500

    feat: Listen for app to b ready and then create new window

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const url = require("url");
 const path = require("path");
 
 const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on("ready", function() {
+  // Create new window
+  mainWindow = new BrowserWindow({});
+});
